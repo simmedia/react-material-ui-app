7ef44666b21a6d0d36fd1c340a7a3f5bebfbd7db
Stefan Simic <simadurlan92@gmail.com>
2020-09-17 02:24:21 +0200

    Router init and Drawer fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+// router
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 // Components
-import { Header } from './components/Header';
-import { Button } from './components/Buttons';
-// import SideNav from './components/Drawer';
+import { Header } from "./components/Header";
+import { Button } from "./components/Buttons";
+
+import Inbox from "./Inbox";
+
+const useStyles = makeStyles((theme) => ({
+  centerElement: {
+    margin: "0 auto",
+    width: "1366px",
+  },
+}));
+
+//  Grid
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <Header lname="Simic"/>
-      {/* <SideNav /> */}
-      <Button />
-    </div>
+    <Router>
+      <div className="App">
+        <Header lname="Simic" />
+        <div
+          className={classes.centerElement}
+          style={{ width: 100 + "%", maxWidth: 1200 + "px" }}
+        >
+          <Grid className={classes.root} container spacing={0}>
+            <Switch>
+              <Route path="/home">
+                <Inbox />
+              </Route>
+            </Switch>
+          </Grid>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Inbox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Inbox = (props) => {
+  return (
+    <div style={{ margin: 100 + "px" }}>
+      <h2>Inbox</h2>
+    </div>
+  );
+};
+
+export default Inbox;
